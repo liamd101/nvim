b14fe1d5b270562fdb33db4356fac8e08ff776b1
liamd101 <liamdodds101@gmail.com>
2023-09-01 16:11:40 -0400

    Removed print statement

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,7 +9,7 @@ lsp.ensure_installed({
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local smp_mappings = lsp.defaults.cmp_mappings({
+local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true}),
@@ -25,7 +25,6 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.on_attach(function(client, bufnr)
-	print("help")
 	local opts = {buffer = bufnr, remap = false}
 
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
